0d343377323743af67b160a3c0bd802b2cdf3442
Geneva Le <gle@BELC02RNA0SG8WN.sea.corp.expecn.com>
2019-11-19 21:59:14 -0800

    update ga-track methods

--- pages/utils/google-analytics/ga-track.js
+++ pages/utils/google-analytics/ga-track.js
@@ -1,28 +1,15 @@
 import ReactGA from 'react-ga';
 
-const isProd = true; //TODO - Track only production analytics otherwise dev and test would be tracked as well
-
 function initializeGoogleAnalytics() {
-    if (!isProd) {
-        return;
-    }
     ReactGA.initialize('UA-33712689-7');
 }
 
 function trackPageView(path) {
-    if (!isProd) {
-        return;
-    }
-
     ReactGA.set({ page: path });
     ReactGA.pageview(path);
 }
 
 function trackEvent(args) {
-    if (!isProd) {
-        return;
-    }
-
     if(!args) {
         return;
     }
